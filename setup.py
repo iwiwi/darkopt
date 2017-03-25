338d5e6f85a78f6c786b52669d4054d7c8a0edfc
Takuya Akiba <t.akiba.65536@gmail.com>
2017-03-25 17:07:05 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='darkopt',
+    version='0.0.1',
+    author='Takuya Akiba',
+    packages=find_packages(),
+)
