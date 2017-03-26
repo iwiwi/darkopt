eb3a80461f39a0e34f977f8191eb2e622bd037cf
Takuya Akiba <t.akiba.65536@gmail.com>
2017-03-26 16:41:16 +0900

    Import more for easier use

--- darkopt/__init__.py
+++ darkopt/__init__.py
@@ -1,3 +1,8 @@
 from darkopt import integration  # NOQA
 from darkopt import learning_curve  # NOQA
 from darkopt import optimize  # NOQA
+
+from darkopt.integration import ChainerTrigger  # NOQA
+from darkopt.integration import XGBoostCallback  # NOQA
+from darkopt.optimize import Optimizer  # NOQA
+from darkopt.optimize import TrialResult  # NOQA
