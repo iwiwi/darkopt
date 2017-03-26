82d14fbda5bad87ad8cb76ed382359a80a43b337
Takuya Akiba <t.akiba.65536@gmail.com>
2017-03-26 16:27:48 +0900

    Enhance Optimizer to support maximization

--- darkopt/optimize/optimizer.py
+++ darkopt/optimize/optimizer.py
@@ -4,16 +4,31 @@ from darkopt.optimize import engine as engine_module
 from darkopt.optimize import trial_result as trial_result_module
 
 
+def _is_better(new_score, known_best_score, maximize):
+    if maximize:
+        return new_score > known_best_score
+    else:
+        return new_score < known_best_score
+
+
+def _get_inf(maximize):
+    if maximize:
+        return -np.inf
+    else:
+        return np.inf
+
+
 class Optimizer(object):
 
-    def __init__(self, target_func, param_space, engine='random_search', prune=True):
+    def __init__(self, target_func, param_space, engine='random_search', maximize=False, prune=True):
         self.target_func = target_func
         self.param_space = param_space
         self.engine = engine_module.get_engine(engine, param_space)
+        self.maximize = maximize
         self.prune = prune
 
         self.trial_results = []
-        self.known_best_trial = trial_result_module.TrialResult(np.inf)
+        self.known_best_trial = trial_result_module.TrialResult(_get_inf(maximize))
 
     def optimize(self, max_n_calls=None):
         while max_n_calls is None or max_n_calls > 0:
@@ -22,15 +37,19 @@ class Optimizer(object):
             if self.prune:
                 known_best_score = self.known_best_trial.score
             else:
-                known_best_score = np.inf
+                known_best_score = _get_inf(self.maximize)
 
             trial_result = self.target_func(param, known_best_score)
             trial_result = trial_result_module.get_trial_result(trial_result)
             trial_result.param = param
+
+            # TODO(iwiwi): engines need to know whether maximizing or minimizing
+            self.engine.report(param, trial_result)
             self.trial_results.append(trial_result)
             print(param, trial_result.score, known_best_score)
 
-            if trial_result.status == 'ok' and trial_result.score < self.known_best_trial.score:
+            if trial_result.status == 'ok' and _is_better(
+                    trial_result.score, self.known_best_trial.score, self.maximize):
                 self.known_best_trial = trial_result
 
             if max_n_calls is not None:
