00ad1c4adb19a595ed960c2ed3d778ada407e8cb
Takuya Akiba <t.akiba.65536@gmail.com>
2017-03-26 15:35:43 +0900

    Remove old random search methods

--- darkopt/optimize/__init__.py
+++ darkopt/optimize/__init__.py
@@ -1,4 +1,2 @@
 from darkopt.optimize.optimizer import Optimizer  # NOQA
-from darkopt.optimize.random_search import naive_random_search  # NOQA
-from darkopt.optimize.random_search import pruned_random_search  # NOQA
 from darkopt.optimize.trial_result import TrialResult  # NOQA
